01d91e2e0f5aaf7e5765d13af9c7b9b471bc7938
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-06 22:11:17 +0100

    feat: Implement a mock API for getting course related info

---
+++ src/util/mockAPI.js
@@ -0,0 +1,89 @@
+import createCourse from "./createCourse";
+import createStudent from "./createStudent"
+import createWeeklyContentInfo from "./createWeeklyContentInfo"
+
+const syllabi = {
+  accessibleHtml: [
+    createWeeklyContentInfo(1, "Meet ARIA (not the Stark girl)", "You will probably learn something usefull"),
+    createWeeklyContentInfo(2, "The Accessibility tree", "You won't believe how cool it is"),
+    createWeeklyContentInfo(3, "The things that are definetly important", "Commiting early and often... ?"),
+  ],  
+  introToHtml: [
+    createWeeklyContentInfo(1, "Hello World!", "Take the first steps"),
+    createWeeklyContentInfo(2, "Semantic HTML", "Use the right tool for the right taks"),
+    createWeeklyContentInfo(3, "Wrap up", "Create a Capstone project hehehe"),
+  ],  
+  fullStack: [
+    createWeeklyContentInfo(1, "Basic to do App", "Start by brushing up on the basics of web dev"),
+    createWeeklyContentInfo(2, "Shopping Cart", "Create a shopping cart app insoired by Amazon's website (or whatever, idk)"),
+  ],  
+}
+
+const students = {
+  groupOne: [
+    createStudent("Tirso Samalungo", "tirso@example.com"),
+    createStudent("Rayanna Maber", "ray@example.com"),
+    createStudent("Kassandra dos Santos", "kassy@example.com"),
+  ],
+  groupTwo: [
+    createStudent("Elsa Torres", "elsa@example.com"),
+    createStudent("Daniela Conrado", "dani@example.com"),
+  ],
+  groupThree: [
+    createStudent("Arminda Rodrigues", "arminda@example.com"),
+    createStudent("Lulena Benjamin", "joa@example.com"),
+  ]
+}
+
+const prerequisites = {
+  beginner: ["None"],
+  intermediate: ["HTML, CSS, Javascript"],
+  advanced: ["HTML, CSS, Javascript, React, Tailwind, Node.js, Express.js"]
+}
+
+const accessibleHtml = createCourse(
+  "Write accessible HTML", 
+  "Josh Something", 
+  "Learn about accessibility features of HTML",
+  "open", 
+  "/accessibleHtml.png",
+  "3 hours",
+  "Any time",
+  "Online",
+  prerequisites.intermediate,
+  syllabi.accessibleHtml,
+  students.groupOne,  
+)
+
+const introToHtml = createCourse(
+  "Intro to HTML", 
+  "Josh More Things", 
+  "Learn about the basic building block of the web, HTML",
+  "closed", 
+  "/introTohtml.png",
+  "1 hour",
+  "Any time",
+  "Online",
+  prerequisites.beginner,
+  syllabi.introToHtml,
+  students.groupThree,  
+)
+
+const fullStack = createCourse(
+  "Full Stack Projects", 
+  "Josh No Things", 
+  "Create stunning full stack applications",
+  "In Progress", 
+  "/fullStack.png",
+  "6 hour",
+  "Any time",
+  "Online",
+  prerequisites.advanced,
+  syllabi.fullStack,
+  students.groupTwo,  
+)
+
+export default function mockAPI() {
+  return [accessibleHtml, introToHtml, fullStack ] 
+}
+
