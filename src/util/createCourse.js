ff22311fd3505feb3a388887bac216288bb70339
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-06 21:28:25 +0100

    refactor: Remove default values for schedule and location parameters

--- src/util/createCourse.js
+++ src/util/createCourse.js
@@ -7,8 +7,8 @@ export default function createCourse(
   enrollmentStatus,
   thumbnail,
   duration,
-  schedule = "Any time",
-  location = "Online",
+  schedule,
+  location,
   prerequisites,
   syllabus,
   students
