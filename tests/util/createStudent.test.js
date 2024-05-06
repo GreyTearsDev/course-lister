1f68f10d5e80b9d1b42fd8c061004c18de5e395d
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-06 20:14:55 +0100

    feat: Create test for createStudent.js function

---
+++ tests/util/createStudent.test.js
@@ -0,0 +1,17 @@
+import createStudent from "../../src/util/createStudent";
+
+describe("createStudent", () => {
+  const student = createStudent("joe", "justan@example.com");
+  
+  it("returns an object with an id, a name, and an email key", () => {
+    expect(student).toHaveProperty("id");
+    expect(student).toHaveProperty("name");
+    expect(student).toHaveProperty("email");
+  })
+
+  it("assigns the values passed as arguments to the correct fields", () => {
+    expect(student.name).toBe("joe");
+    expect(student.email).toBe("justan@example.com");
+  })
+
+})
