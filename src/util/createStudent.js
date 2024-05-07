d1cbc7121f7b8cb602798cf0e2a7d54f8fa06fb6
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-07 10:47:56 +0100

    refactor: Refactor createStudent to include methods for adding, removing, and getting course IDs

--- src/util/createStudent.js
+++ src/util/createStudent.js
@@ -1,9 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default function createStudent(name, email) {
+  const enrolledCorsesIDs = [];
+
+  const addCourse = (course) => enrolledCorsesIDs.push(course.id);
+  const removeCourse = (course) => enrolledCorsesIDs.filter(existingID => existingID !== course.id);
+  const getCoursesIDs = () => enrolledCorsesIDs;
+  
   return {
     id: uuidv4(),
     name: name,
-    email: email
+    email: email,
+    addCourse,
+    removeCourse,
+    getCoursesIDs,
   }
 }
