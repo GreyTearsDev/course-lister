0e5f7fc7fa1450d095315ac2641db94edd9cdc19
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-08 21:02:50 +0100

    refactor: Integrate Redux into the application for managing state

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import routes from "./routes.jsx";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
