c1632f87832a68bdbab6a6911695cce5ec03dbcc
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-08 12:34:24 +0100

    Delete unused enrollInCourse and findCourse functions

--- src/util/enrollInCourse.js
+++
@@ -1,4 +0,0 @@
-export default function enrollInCourse(student, course) {
-  student.addCourse(course.id);
-  course.students.push(student);
-}
--- src/util/findCourse.js
+++
@@ -1,5 +0,0 @@
-import {mockAPI} from "./mockAPI";
-
-export default function findCourse(courseID) {
-  return mockAPI.find(element => element.id === courseID);  
-}
