8e744f41bd56373c12bde43f49529ffd718501fa
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-08 05:18:52 +0100

    Add babel to the array of plugins

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    babel()
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
