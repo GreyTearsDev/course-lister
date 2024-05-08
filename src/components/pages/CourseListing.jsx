43b113fa8d20faf2622d4cb2ccd33b78fa394346
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-08 21:00:17 +0100

    refactor: Refactor CourseListing component to use Redux for managing course data
    import selectGetCourses from the redux reducers and used useSelector to get courses from the Redux store. Also, now the component renders CourseDetailsScreen for each course

--- src/components/pages/CourseListing.jsx
+++ src/components/pages/CourseListing.jsx
@@ -1,3 +1,14 @@
+import { useSelector } from "react-redux";
+import { selectGetCourses } from "../../redux/reducers";
+import CourseDetailsScreen from "./CourseDetailsScreen";
+
 export default function CourseListing() {
-  return <h1>These are the courses h</h1>;
+  const courses = useSelector(selectGetCourses);
+  return (
+    <>
+      <div>
+        {courses.map(course => <CourseDetailsScreen key={course.id} course={course} />)}
+      </div>
+    </>
+  );
 }
