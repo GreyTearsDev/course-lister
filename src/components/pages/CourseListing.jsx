cc00e03d0b370bf2c4fddcdadfe72412c7424e84
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-10 00:35:04 +0100

    feat: Implement course search functionality
    added search functionality to CourseListing component, allowing users to filter courses by name. The  state is updated based on user input, and courses are filtered based on the query

--- src/components/pages/CourseListing.jsx
+++ src/components/pages/CourseListing.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectGetCourses } from "../../redux/reducers";
 import CourseDetailsScreen from "./CourseDetailsScreen";
 
 export default function CourseListing() {
   const courses = useSelector(selectGetCourses);
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const getFilteredCourses = (query, courses) => {
+    if (!query) return courses;
+    return courses.filter(course => course.name.toLowerCase().includes(query.toLowerCase()));
+  };
+
+  const filteredCourses = getFilteredCourses(query, courses);
+
   return (
     <div className="screen">
-      {courses.map(course => <CourseDetailsScreen key={course.id} course={course} />)}
+      <div className="search-bar">
+        <label htmlFor="search">
+          Search<input type="text" onChange={handleSearch} />
+        </label>
+      </div>
+      {filteredCourses.map(course => <CourseDetailsScreen key={course.id} course={course} />)}
     </div>
   );
 }
