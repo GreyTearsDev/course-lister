764e6684bd0e388558a5fa0f799cad24a32261c4
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 22:29:28 +0100

    refactor: Update the appearance order of the 'enroll' button

--- src/components/pages/CourseMainInfo.jsx
+++ src/components/pages/CourseMainInfo.jsx
@@ -3,12 +3,6 @@ import PropTypes from "prop-types";
 export default function CourseMainInfo(props) {
   return (
     <div className="info__main">
-      <button
-        className="btn btn--large"
-        onClick={props.handleEnroll}
-      >
-        {!props.enroll ? "Enroll" : "Drop out"}
-      </button>
       <h2>{props.course.name}</h2>
       <h4>Instructor: {props.course.instructor}</h4>
       <h4>Duration: {props.course.duration}</h4>
@@ -16,6 +10,12 @@ export default function CourseMainInfo(props) {
       <h4>
         Prerequisites: {props.course.prerequisites.join(", ")}
       </h4>
+      <button
+        className="btn btn--large"
+        onClick={props.handleEnroll}
+      >
+        {!props.enroll ? "Enroll" : "Drop out"}
+      </button>
       <p>{props.course.description}</p>
     </div>
   );
