705929d7ee02db9e15246c97dd46abd0076423af
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 08:30:59 +0100

    refactor: Refactor CourseMainInfo component button rendering

--- src/components/pages/CourseMainInfo.jsx
+++ src/components/pages/CourseMainInfo.jsx
@@ -3,14 +3,12 @@ import PropTypes from "prop-types";
 export default function CourseMainInfo(props) {
   return (
     <div className="info__main">
-      {props.expandCourse ? null : (
-        <button
-          className="btn btn--large"
-          onClick={props.handleEnroll}
-        >
-          {!props.enroll ? "Enroll" : "Drop out"}
-        </button>
-      )}
+      <button
+        className="btn btn--large"
+        onClick={props.handleEnroll}
+      >
+        {!props.enroll ? "Enroll" : "Drop out"}
+      </button>
       <h2>{props.course.name}</h2>
       <h4>Instructor: {props.course.instructor}</h4>
       <h4>Duration: {props.course.duration}</h4>
