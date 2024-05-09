c8b23152cbf0b5761a419794231149079d6f1216
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 22:02:43 +0100

    feat: Fetch API data on App component mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { fetchAPIData } from "./redux/reducers";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAPIData());
+  }, [dispatch]);
+
   return (
     <div className="app">
       <Navbar />
