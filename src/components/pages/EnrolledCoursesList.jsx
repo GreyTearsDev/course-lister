18cfa882329dfc4c5ee7c5d934424373a47836d1
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-10 00:10:59 +0100

    refactor: Update EnrolledCoursesList to use EnrolledCourse component

--- src/components/pages/EnrolledCoursesList.jsx
+++ src/components/pages/EnrolledCoursesList.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { setCompletionStatus } from "../../redux/reducers";
+import EnrolledCourse from "./EnrolledCourse";
 
 export default function EnrolledCoursesList(props) {
   const dispatch = useDispatch();
@@ -20,29 +21,7 @@ export default function EnrolledCoursesList(props) {
         : props.courses.map(course => {
           if (!props.user.enrolledCoursesIds.includes(course.id)) return;
 
-          return (
-            <div
-              className={`course course--collapsed ${course.isCompleted ? "course--completed" : null}`}
-              key={course.id}
-            >
-              <div className="thumbnail">
-                <img src={course.thumbnail} alt="" />
-              </div>
-              <div className="info">
-                <h2>Title: {course.name}</h2>
-                <h3>Instructor: {course.instructor}</h3>
-                <label htmlFor="completion">
-                  Completed
-                  <input
-                    checked={course.isCompleted}
-                    type="checkbox"
-                    id="completion"
-                    onChange={() => handleCourseCompletion(course)}
-                  />
-                </label>
-              </div>
-            </div>
-          );
+          return <EnrolledCourse key={course.id} course={course} handleCourseCompletion={handleCourseCompletion} />;
         })}
     </div>
   );
