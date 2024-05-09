f39ef9d2ba2d9cfdc7c51ee586476e38502ad867
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 21:08:50 +0100

    refactor: Update Home component to include a link to course listing
    added a Link component from React Router to create a link to the '/course-listing' route, and
    updated the return statement to include the Link component.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Welcome to Course Lister</h1>;
+  return (
+    <>
+      <h1>Welcome to Course Lister</h1>
+      <h3>
+        Check out our courses 👉 <Link to="/course-listing">HERE</Link>
+      </h3>
+    </>
+  );
 }
