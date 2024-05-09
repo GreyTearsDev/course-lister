cfbc2687a45cf54f56be22e0bb08d9aecc0dff83
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 06:46:41 +0100

    refactor: Refactor createStudent test
    removed unnecessary imports and test cases for the 'addCourse', 'removeCourse', and 'getCoursesIDs' properties. The test now focuses on ensuring that the function returns an object with the expected keys and assigns the values passed as arguments to the correct fields.

--- tests/util/createStudent.test.js
+++ tests/util/createStudent.test.js
@@ -1,5 +1,5 @@
 import createStudent from "../../src/util/createStudent";
-import {describe, it, expect, vi, beforeAll, beforeEach} from "vitest";
+import {describe, it, expect,  beforeAll, } from "vitest";
 
 
 describe("createStudent", () => {
@@ -13,9 +13,6 @@ describe("createStudent", () => {
     expect(student).toHaveProperty("id");
     expect(student).toHaveProperty("name");
     expect(student).toHaveProperty("email");
-    expect(student).toHaveProperty("addCourse");
-    expect(student).toHaveProperty("removeCourse");
-    expect(student).toHaveProperty("getCoursesIDs");
   })
 
   it("assigns the values passed as arguments to the correct fields", () => {
@@ -25,53 +22,4 @@ describe("createStudent", () => {
  
 })
 
-describe("addCourse", () => {
-  let student;
-  let courseMock;
-  let addCourseSpy;
-
-  beforeEach(() => {
-    student = createStudent("joe", "justan@example.com");
-    courseMock = { id: "1" };
-    addCourseSpy = vi.spyOn(student, "addCourse");
-  });
-
-  it("gets correctly called called", () => {
-    student.addCourse(courseMock);
-    expect(student.addCourse).toHaveBeenCalled();
-    addCourseSpy.mockRestore();
-  })
-
-  it("saves the id of the desired course to the array of course ids", () => {
-    student.addCourse(courseMock);
-    expect(student.getCoursesIDs()).toContain(courseMock.id);
-    addCourseSpy.mockRestore();
-  })
-})
-
-describe("removeCourse", () => {
-  let student;
-  let courseMock;
-  let removeCourseSpy;
-
-  beforeEach(() => {
-    student = createStudent("joe", "justan@example.com");
-    courseMock = { id: "1" };
-    removeCourseSpy = vi.spyOn(student, "removeCourse");
-  });
-
-  it("gets correctly called called", () => {
-    student.removeCourse(courseMock);
-    expect(student.removeCourse).toHaveBeenCalled();
-    removeCourseSpy.mockRestore();
-  })
   
-  it("remove the id of the desired course from the array of course ids", () => {
-    student.removeCourse(courseMock);
-    expect(student.getCoursesIDs()).not.toContain(courseMock.id);
-    removeCourseSpy.mockRestore();
-  })
-
-  
-  
-})
