c05d83773ec5cdf941c369eef745d1fa00da8c1d
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 21:06:58 +0100

    feat: Add getCourse and setCompletionStatus actions
    added getCourse action to retrieve a course by ID, and added setCompletionStatus action to set the completion status of a course.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,22 @@
 export const TOGGLE_ENROLL= "TOGGLE_ENROLL";
+export const GET_COURSE = "GET_COURSE";
 export const REMOVE_COURSE_FROM_USER = "REMOVE_COURSE_FROM_USER";
 export const ADD_COURSE_TO_USER = "ADD_COURSE_TO_USER";
+export const SET_COMPLETION_STATUS = "SET_COMPLETION_STATUS";
 
 export const toggleEnroll = (courseId) => ({
   type: TOGGLE_ENROLL,
   payload:  courseId ,
+});
+
+export const getCourse = (courseId) => ({
+  type: GET_COURSE,
+  payload:  courseId ,
+});
+
+export const setCompletionStatus = (course) => ({
+  type: SET_COMPLETION_STATUS,
+  payload:  course,
 });
 
 export const removeCourseFromUser = (courseId) => ({
