33cfb5475269e85e2a0dc5dd70341626331c3c0b
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 21:17:48 +0100

    refactor: Render the dashboard screen, displaying a welcome message and a list of enrolled courses.

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,3 +1,18 @@
+import { useSelector } from "react-redux";
+import { selectGetCourses, selectUser } from "../../redux/reducers";
+import EnrolledCoursesList from "./EnrolledCoursesList";
+
 export default function Dashboard() {
-  return <h1>Student Dashboard</h1>;
+  const user = useSelector(selectUser);
+  const enrolledCoursesIds = user.enrolledCoursesIds;
+
+  const courses = useSelector(selectGetCourses);
+
+  return (
+    <div className="screen dashboard">
+      <h1>Welcome back, {user.name}</h1>
+      <h3>Your courses:</h3>
+      <EnrolledCoursesList enrolledCoursesIds={enrolledCoursesIds} courses={courses} user={user} />
+    </div>
+  );
 }
