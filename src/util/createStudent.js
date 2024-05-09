2fe59eb1a758ad67afb9253a835be604dfd0ab19
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 06:51:11 +0100

    refactor: Refactor createStudent function
    simplified the structure. Now, instead of having separate methods for adding, removing, and getting course IDs, it now directly includes an array for enrolled course IDs.

--- src/util/createStudent.js
+++ src/util/createStudent.js
@@ -2,17 +2,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function createStudent(name, email) {
   const enrolledCorsesIDs = [];
-
-  const addCourse = (course) => enrolledCorsesIDs.push(course.id);
-  const removeCourse = (course) => enrolledCorsesIDs.filter(existingID => existingID !== course.id);
-  const getCoursesIDs = () => enrolledCorsesIDs;
-  
   return {
     id: uuidv4(),
     name: name,
     email: email,
-    addCourse,
-    removeCourse,
-    getCoursesIDs,
+    enrolledCorsesIDs
   }
 }
