e9bb34f7d1e30d8403f90380cf75be9e459e2bcb
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-09 21:13:19 +0100

    feat: Implement EnrroleCoursesList component
    it renders a list of enrolled courses with the option to mark them as completed.

---
+++ src/components/pages/EnrolledCoursesList.jsx
@@ -0,0 +1,50 @@
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { setCompletionStatus } from "../../redux/reducers";
+
+export default function EnrolledCoursesList(props) {
+  const dispatch = useDispatch();
+
+  const handleCourseCompletion = (course) => {
+    dispatch(setCompletionStatus(course));
+  };
+  return (
+    <div className="enrolled-courses">
+      {props.enrolledCoursesIds.length === 0
+        ? (
+          <h3>
+            You have not enrolled in any course yet. Find one <Link to="/course-listing">HERE</Link>
+          </h3>
+        )
+        : props.courses.map(course => {
+          if (!props.user.enrolledCoursesIds.includes(course.id)) return;
+
+          return (
+            <div
+              className={`course course--collapsed ${course.isCompleted ? "course--completed" : null}`}
+              key={course.id}
+            >
+              <div className="thumbnail">
+                <img src={course.thumbnail} alt="" />
+              </div>
+              <div className="info">
+                <h2>{course.name}</h2>
+                <h3>{course.instructor}</h3>
+                <label htmlFor="completion">
+                  Completed
+                  <input type="checkbox" id="completion" onChange={() => handleCourseCompletion(course)} />
+                </label>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+EnrolledCoursesList.propTypes = {
+  enrolledCoursesIds: PropTypes.arrayOf(PropTypes.string),
+  courses: PropTypes.arrayOf(PropTypes.object),
+  user: PropTypes.object,
+};
