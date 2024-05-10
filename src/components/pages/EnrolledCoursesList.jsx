2ff83d41f92318e9a31b4ccfbed5abc7c9168adb
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-10 06:35:12 +0100

    refactor: Update the component for consistent mapping over courses and rendering the EnrolledCourse component

--- src/components/pages/EnrolledCoursesList.jsx
+++ src/components/pages/EnrolledCoursesList.jsx
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { setCompletionStatus } from "../../redux/reducers";
 import EnrolledCourse from "./EnrolledCourse";
 
@@ -12,17 +11,11 @@ export default function EnrolledCoursesList(props) {
   };
   return (
     <div className="enrolled-courses">
-      {props.enrolledCoursesIds.length === 0
-        ? (
-          <h3>
-            You have not enrolled in any course yet. Find one <Link to="/course-listing">HERE</Link>
-          </h3>
-        )
-        : props.courses.map(course => {
-          if (!props.user.enrolledCoursesIds.includes(course.id)) return;
+      {props.courses.map(course => {
+        if (!props.user.enrolledCoursesIds.includes(course.id)) return;
 
-          return <EnrolledCourse key={course.id} course={course} handleCourseCompletion={handleCourseCompletion} />;
-        })}
+        return <EnrolledCourse key={course.id} course={course} handleCourseCompletion={handleCourseCompletion} />;
+      })}
     </div>
   );
 }
