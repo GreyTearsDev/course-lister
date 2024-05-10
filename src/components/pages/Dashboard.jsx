42599bb1910b3de868cdffa506187d2a4093725f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-10 06:36:44 +0100

    refactor: Conditionally render a centered message if the user is not enrolled in any courses.

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,18 +1,29 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { selectGetCourses, selectUser } from "../../redux/reducers";
 import EnrolledCoursesList from "./EnrolledCoursesList";
 
 export default function Dashboard() {
   const user = useSelector(selectUser);
-  const enrolledCoursesIds = user.enrolledCoursesIds;
+  const isEnrolledInCourses = user.enrolledCoursesIds.length === 0;
 
   const courses = useSelector(selectGetCourses);
 
   return (
-    <div className="screen dashboard">
+    <div className={`screen dashboard ${isEnrolledInCourses ? "centered-message" : null}`}>
       <h1>Welcome back, {user.name}</h1>
-      <h3>Your courses:</h3>
-      <EnrolledCoursesList enrolledCoursesIds={enrolledCoursesIds} courses={courses} user={user} />
+      {isEnrolledInCourses
+        ? (
+          <h3>
+            You have not enrolled in any course yet. Find one 👉 <Link to="/course-listing">HERE</Link>
+          </h3>
+        )
+        : (
+          <>
+            <h3>Your courses:</h3>
+            <EnrolledCoursesList courses={courses} user={user} />
+          </>
+        )}
     </div>
   );
 }
