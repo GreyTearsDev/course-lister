c4024ce331eeecfbde4c2e3d1344e983515600d8
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-05-10 06:32:10 +0100

    refactor: Update Home component to use a centered message
    updated it to use a <div> with a class name of 'home centered-message' instead of a fragment, for better structure and styling consistency.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
+    <div className="home centered-message">
       <h1>Welcome to Course Lister</h1>
       <h3>
         Check out our courses 👉 <Link to="/course-listing">HERE</Link>
       </h3>
-    </>
+    </div>
   );
 }
